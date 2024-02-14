e198b94006dd547bc688f93619d9863c01a95a06
WizkidXIV <152577087+WizkidXIV@users.noreply.github.com>
2024-02-13 18:27:07 -0800

    completed assignment

--- public/index.js
+++ public/index.js
@@ -1,46 +1,104 @@
-async function main() {
-
-    let response = await fetch('http://localhost:3001/listBooks')
-    let books = await response.json()
-
-    books.forEach(renderBook)
-
-    // Update book
-    let updateResponse = await fetch('http://localhost:3001/updateBook', {
-    method: "PATCH",
-    headers: {
-        'content-type': 'application/json'
-    },
-    body: JSON.stringify({
-        "id": 3,
-        "title": "Legends of Arathrae",
-    }),
-});
-let updatedBook = await updateResponse.json();
-console.log(updatedBook);
+function getColor(stock) {
+    if (stock === "GME") {
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if (stock === "MSFT") {
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if (stock === "DIS") {
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if (stock === "BNTX") {
+        return 'rgba(166, 43, 158, 0.7)'
+    }
 }
 
-function renderBook(book) {
-    let bookContainer = document.querySelector('.book-container')
-    bookContainer.innerHTML += `
-        <div class="ui card">
-            ${book.imageURL ? `
-                <div class="image">
-                    <img src="${book.imageURL}" />
-                </div>
-            `
-            : ``}
-            <div class="content">
-                <a class="header">${book.title}</a>
-                <div class="meta">
-                    <span class="date">Available: ${book.quantity}</span>
-                </div>
-                <div class="description">
-                    ${book.description}
-                </div>
-            </div>
-        </div>
-    `
+async function main() {
+    const timeChartCanvas = document.querySelector('#time-chart');
+    const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
+    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+
+    const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=b2b0e11651294ee9a789558a1625b754`)
+
+    const result = await response.json()
+
+    const { GME, MSFT, DIS, BNTX } = result;
+
+    const stocks = [GME, MSFT, DIS, BNTX];
+
+    stocks.forEach(stock => stock.values.reverse())
+
+    // Time Chart
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symbol,
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+                data: stock.values.map(value => parseFloat(value.high))
+            }))
+        }
+    });
+
+    // High Chart
+    new Chart(highestPriceChartCanvas.getContext('2d'), {
+        type: 'bar',
+        data: {
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [{
+                label: 'Highest',
+                backgroundColor: stocks.map(stock => (
+                    getColor(stock.meta.symbol)
+                )),
+                borderColor: stocks.map(stock => (
+                    getColor(stock.meta.symbol)
+                )),
+                data: stocks.map(stock => (
+                    findHighest(stock.values)
+                ))
+            }]
+        }
+    });
+
+    // Average Chart
+    new Chart(averagePriceChartCanvas.getContext('2d'), {
+        type: 'pie',
+        data: {
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [{
+                label: 'Average',
+                backgroundColor: stocks.map(stock => (
+                    getColor(stock.meta.symbol)
+                )),
+                borderColor: stocks.map(stock => (
+                    getColor(stock.meta.symbol)
+                )),
+                data: stocks.map(stock => (
+                    calculateAverage(stock.values)
+                ))
+            }]
+        }
+    });
+}
+
+function findHighest(values) {
+    let highest = 0;
+    values.forEach(value => {
+        if (parseFloat(value.high) > highest) {
+            highest = value.high
+        }
+    })
+    return highest
+}
+
+function calculateAverage(values) {
+    let total = 0;
+    values.forEach(value => {
+        total += parseFloat(value.high)
+    })
+    return total / values.length
 }
 
 main()
